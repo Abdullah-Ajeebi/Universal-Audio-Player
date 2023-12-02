0b027c95214ee46f7c72b7fdeba95ab0f830e5bd
abdul <abdullah.ajeebi19@gmail.com>
2023-12-02 17:18:32 +0300

    Time formatting

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,7 +16,26 @@ namespace UniversalAudioPlayer {
         }
 
         private void Tick_Tick(object sender, EventArgs e) {
-            AudioLengthTablePanel.Size = new System.Drawing.Size(ClientSize.Width - 24, 100);
+            string hours, minutes, seconds, fractionalSeconds;
+            SecondsToHHMMSS(Convert.ToSingle(textBox1.Text), out hours, out minutes, out seconds, out fractionalSeconds);
+            TimeLabel.Text = $"{hours}:{minutes}:{seconds}{fractionalSeconds}{Environment.NewLine}/{Environment.NewLine}0: 0:00.00";
+        }
+
+        public void SecondsToHHMMSS(float secondsIn, out string hoursOut, out string minutesOut, out string secondsOut, out string fractionalSecondsOut) {
+            int hours = (int)Convert.ToSingle(secondsIn) / 3600;
+            int remainingSeconds = (int)Convert.ToSingle(secondsIn) % 3600;
+            int minutes = remainingSeconds / 60;
+            int seconds = remainingSeconds % 60;
+            float fractionalSeconds = secondsIn - (int)secondsIn;
+            fractionalSecondsOut = fractionalSeconds.ToString(".##");
+            minutesOut = minutes.ToString("00");
+            secondsOut = seconds.ToString("00");
+            hoursOut = hours.ToString();
+        }
+
+        private void DoTimeResize(object sender, EventArgs e) {
+            TimePanel.Size = new Size(ClientSize.Width - 20, 100);
+            TimeLabel.Location = new Point((TimePanel.Size.Width / 2) - TimeLabel.Size.Width / 2, 12);
         }
     }
 }

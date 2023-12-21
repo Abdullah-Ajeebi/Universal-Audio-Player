2d3cd50dc54169e51c6751cba8836d9b91dd4e5b
abdul <abdullah.ajeebi19@gmail.com>
2023-12-21 14:27:10 +0300

    Made it play audio files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace UniversalAudioPlayer {
-    partial class Form1 {
+    partial class MainForm {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -24,11 +24,23 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.Tick = new System.Windows.Forms.Timer(this.components);
             this.TimeLabel = new System.Windows.Forms.Label();
             this.TimePanel = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.audioPlayerAddonsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.TimePanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Tick
@@ -42,7 +54,7 @@
             this.TimeLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.TimeLabel.AutoSize = true;
             this.TimeLabel.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TimeLabel.Location = new System.Drawing.Point(59, 12);
+            this.TimeLabel.Location = new System.Drawing.Point(44, 11);
             this.TimeLabel.Name = "TimeLabel";
             this.TimeLabel.Size = new System.Drawing.Size(109, 75);
             this.TimeLabel.TabIndex = 0;
@@ -52,7 +64,7 @@
             // TimePanel
             // 
             this.TimePanel.Controls.Add(this.TimeLabel);
-            this.TimePanel.Location = new System.Drawing.Point(12, 24);
+            this.TimePanel.Location = new System.Drawing.Point(12, 27);
             this.TimePanel.Name = "TimePanel";
             this.TimePanel.Size = new System.Drawing.Size(200, 100);
             this.TimePanel.TabIndex = 0;
@@ -60,28 +72,114 @@
             // textBox1
             // 
             this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.textBox1.Location = new System.Drawing.Point(51, 234);
+            this.textBox1.Location = new System.Drawing.Point(61, 176);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "60";
             // 
-            // Form1
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = resources.GetString("openFileDialog.Filter");
+            this.openFileDialog.Title = "Open File";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripComboBox1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(224, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openItem,
+            this.playItem,
+            this.exitItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openItem
+            // 
+            this.openItem.Image = global::UniversalAudioPlayer.Properties.Resources.OpenFile;
+            this.openItem.Name = "openItem";
+            this.openItem.Size = new System.Drawing.Size(180, 22);
+            this.openItem.Text = "Open";
+            this.openItem.Click += new System.EventHandler(this.openItem_Click);
+            // 
+            // playItem
+            // 
+            this.playItem.Image = ((System.Drawing.Image)(resources.GetObject("playItem.Image")));
+            this.playItem.Name = "playItem";
+            this.playItem.Size = new System.Drawing.Size(180, 22);
+            this.playItem.Text = "Play";
+            // 
+            // exitItem
+            // 
+            this.exitItem.Image = global::UniversalAudioPlayer.Properties.Resources.Exit;
+            this.exitItem.Name = "exitItem";
+            this.exitItem.Size = new System.Drawing.Size(180, 22);
+            this.exitItem.Text = "Exit";
+            this.exitItem.Click += new System.EventHandler(this.exitItem_Click);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsItem,
+            this.audioPlayerAddonsToolStripMenuItem});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(46, 20);
+            this.toolStripComboBox1.Text = "Tools";
+            // 
+            // optionsItem
+            // 
+            this.optionsItem.Name = "optionsItem";
+            this.optionsItem.Size = new System.Drawing.Size(190, 22);
+            this.optionsItem.Text = "Options";
+            this.optionsItem.Click += new System.EventHandler(this.optionsItem_Click);
+            // 
+            // audioPlayerAddonsToolStripMenuItem
+            // 
+            this.audioPlayerAddonsToolStripMenuItem.Name = "audioPlayerAddonsToolStripMenuItem";
+            this.audioPlayerAddonsToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.audioPlayerAddonsToolStripMenuItem.Text = "Audio Player Add-ons";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(224, 361);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.TimePanel);
+            this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Tahoma", 8F);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.Name = "Form1";
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
             this.Text = "Audio Player";
             this.Load += new System.EventHandler(this.DoTimeResize);
             this.Resize += new System.EventHandler(this.DoTimeResize);
             this.TimePanel.ResumeLayout(false);
             this.TimePanel.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +190,16 @@
         private System.Windows.Forms.Label TimeLabel;
         private System.Windows.Forms.Panel TimePanel;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openItem;
+        private System.Windows.Forms.ToolStripMenuItem playItem;
+        private System.Windows.Forms.ToolStripMenuItem exitItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem optionsItem;
+        private System.Windows.Forms.ToolStripMenuItem audioPlayerAddonsToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,16 +8,23 @@ using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using NAudio.Wave;
 
 namespace UniversalAudioPlayer {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class MainForm : Form {
+        public MainForm() {
             InitializeComponent();
+            string extPath = Environment.CurrentDirectory + "\\extensions";
+            if (!Directory.Exists(extPath)) {
+                Directory.CreateDirectory(extPath);
+            }
         }
 
         private void Tick_Tick(object sender, EventArgs e) {
             SecondsToHHMMSS(Convert.ToSingle(textBox1.Text), out string hours, out string minutes, out string seconds, out string fractionalSeconds);
-            TimeLabel.Text = $"{hours}:{minutes}:{seconds}{fractionalSeconds}{Environment.NewLine}/{Environment.NewLine}0: 0:00.00";
+            TimeLabel.Text = $"{hours}:{minutes}:{seconds}{fractionalSeconds}{Environment.NewLine}/{Environment.NewLine}0:0:00.00";
         }
 
         public void SecondsToHHMMSS(float secondsIn, out string hoursOut, out string minutesOut, out string secondsOut, out string fractionalSecondsOut) {
@@ -35,6 +42,49 @@ namespace UniversalAudioPlayer {
         private void DoTimeResize(object sender, EventArgs e) {
             TimePanel.Size = new Size(ClientSize.Width - 20, 100);
             TimeLabel.Location = new Point((TimePanel.Size.Width / 2) - TimeLabel.Size.Width / 2, 12);
+        }
+
+        private void openItem_Click(object sender, EventArgs e) {
+            openFileDialog.ShowDialog(this);
+        }
+
+        private void exitItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void optionsItem_Click(object sender, EventArgs e) {
+            OptionsForm optionsForm = new OptionsForm();
+            optionsForm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var audioFile = new AudioFileReader(openFileDialog.FileName);
+            SoundPlayer soundPlayer = new SoundPlayer(openFileDialog.OpenFile());
+            soundPlayer.Play();
+            wait((int)(audioFile.Length / (audioFile.WaveFormat.SampleRate * audioFile.WaveFormat.Channels * (audioFile.WaveFormat.BitsPerSample / 8))));
+            soundPlayer.Dispose();
+        }
+
+        public void wait(int milliseconds) {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0)
+                return;
+
+            // Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Console.WriteLine("stop wait timer");
+            };
+
+            while (timer1.Enabled) {
+                Application.DoEvents();
+            }
         }
     }
 }
